aca6f9adf8b35ecf42626a83331c6563015337f2
MutantLullaby <MutantLullaby@users.noreply.github.com>
2016-03-31 10:31:40 -0400

    Uploaded

---
+++ gobal-master/gobal/js/scripts.js
@@ -0,0 +1,27 @@
+/* slider */
+
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+ 
+  $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+
+  $('.carousel').carousel ();
+
+});
+
+ $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+        
+            $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
